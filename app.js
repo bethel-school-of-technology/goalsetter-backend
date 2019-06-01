3208539478cf85346e3d37284c225b2abfa19ee7
Dana <stockhoffd@gmail.com>
2019-06-01 11:59:22 -0700

    mysql

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,4 +18,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'your_user',
+    password: 'some_secret',
+    database: 'the_app_database'
+  })
+  
+  connection.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected...')
+  })
 module.exports = app;
