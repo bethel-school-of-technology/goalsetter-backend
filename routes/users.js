57bf1a9afb01f4da719b1dd60e5a61ed867983ab
Dana <stockhoffd@gmail.com>
2019-06-02 19:10:52 -0700

    data available to front-end

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  models.user.findAll ({}).then (foundUsers => {
+    const mappedUsers = foundUsers.map (user =>
+      ({
+        UserID: user.user_id,
+        Name: `${user.first_name} ${user.last_name}`
+      }));
+  res.send(JSON.stringify(mappedUsers));
 });
-
+});
 module.exports = router;
