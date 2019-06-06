879c84101d6a8e7040ae30db5a7267514d9a420d
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-05 23:37:25 -0500

    Commented Out Some Code!

--- routes/index.js
+++ routes/index.js
@@ -27,30 +27,30 @@ router.get('/staticGoals', function (req, res, next) {
 //  })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
- });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+//  });
 
-router.post('/', function(req, res, next) {
-  models.users
-    .findOrCreate({
-      where: {
-        Username: req.body.username
-      },
-      defaults: {
-        FirstName: req.body.firstName,
-        LastName: req.body.lastName,
-        Email: req.body.email,
-        Password: req.body.password
-      }
-    })
-    .spread(function(result, created) {
-      if (created) {
-        res.send('User successfully created');
-      } else {
-        res.send('This user already exists');
-      }
-    });
-});
+// router.post('/', function(req, res, next) {
+//   models.users
+//     .findOrCreate({
+//       where: {
+//         Username: req.body.username
+//       },
+//       defaults: {
+//         FirstName: req.body.firstName,
+//         LastName: req.body.lastName,
+//         Email: req.body.email,
+//         Password: req.body.password
+//       }
+//     })
+//     .spread(function(result, created) {
+//       if (created) {
+//         res.send('User successfully created');
+//       } else {
+//         res.send('This user already exists');
+//       }
+//     });
+// });
 
 module.exports = router;
