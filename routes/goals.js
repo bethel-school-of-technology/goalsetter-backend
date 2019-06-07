e122dd7b9b28a497c1724754ff0fcee50a70a866
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-06 20:51:25 -0500

    Cleaned up Routes to make more sense.

--- routes/goals.js
+++ routes/goals.js
@@ -6,7 +6,7 @@ var staticGoals = require('../staticGoals/goals');
 
 router.get('/staticGoals', function (req, res, next) {
   res.send(JSON.stringify(
-    staticGoals.goals));
+    staticGoals.goal));
 });
 
 router.get('/', function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,35 @@ var router = express.Router();
 //var mysql = require('mysql2');
 var models = require('../models');
 
-var staticGoals = require('../staticGoals/goals');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+ });
 
-router.get('/staticGoals', function (req, res, next) {
-  res.send(JSON.stringify(
-    staticGoals.goal));
+router.post('/', function(req, res, next) {
+  models.users
+    .findOrCreate({
+      where: {
+        Username: req.body.username
+      },
+      defaults: {
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName,
+        Email: req.body.email,
+        Password: req.body.password
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        res.send('User successfully created');
+      } else {
+        res.send('This user already exists');
+      }
+    });
 });
+
+module.exports = router;
+
 
 // var connection = mysql.createConnection({
 //   host: 'localhost',
@@ -16,9 +39,6 @@ router.get('/staticGoals', function (req, res, next) {
 //   password: 'Password1!',
 //   database: 'goalsetter'
 // });
-
-
-
 //  connection.connect(function(err) {
 //    if (err) {
 //      console.error(err.message);
@@ -26,32 +46,3 @@ router.get('/staticGoals', function (req, res, next) {
 //    }
 //    console.log('Yay! You are connected to the database!');
 //  })
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-//  });
-
-// router.post('/', function(req, res, next) {
-//   models.users
-//     .findOrCreate({
-//       where: {
-//         Username: req.body.username
-//       },
-//       defaults: {
-//         FirstName: req.body.firstName,
-//         LastName: req.body.lastName,
-//         Email: req.body.email,
-//         Password: req.body.password
-//       }
-//     })
-//     .spread(function(result, created) {
-//       if (created) {
-//         res.send('User successfully created');
-//       } else {
-//         res.send('This user already exists');
-//       }
-//     });
-// });
-
-module.exports = router;
