3f049a449f78522039bdc78fc84c762977dc67f3
Dana <stockhoffd@gmail.com>
2019-06-08 11:12:51 -0700

    displaying planets on frontend

--- routes/goals.js
+++ routes/goals.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 //var mysql = require('mysql2');
 var models = require('../models');
-var staticGoals = require('../staticGoals/goals');
+var staticGoals = require('../staticModels/goals');
 
 router.get('/staticGoals', function (req, res, next) {
   res.send(JSON.stringify(
--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,42 @@ var express = require('express');
 var router = express.Router();
 //var mysql = require('mysql2');
 var models = require('../models');
+var staticModels = require('../staticModels/planets');
+
+router.get('/staticPlanets', function (req, res, next) {
+
+  res.send(JSON.stringify(
+    staticModels.planet
+  ));
+});
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
- });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+//  });
 
-router.post('/', function(req, res, next) {
-  models.users
-    .findOrCreate({
-      where: {
-        Username: req.body.username
-      },
-      defaults: {
-        FirstName: req.body.firstName,
-        LastName: req.body.lastName,
-        Email: req.body.email,
-        Password: req.body.password
-      }
-    })
-    .spread(function(result, created) {
-      if (created) {
-        res.send('User successfully created');
-      } else {
-        res.send('This user already exists');
-      }
-    });
-});
+// router.post('/', function(req, res, next) {
+//   models.users
+//     .findOrCreate({
+//       where: {
+//         Username: req.body.username
+//       },
+//       defaults: {
+//         FirstName: req.body.firstName,
+//         LastName: req.body.lastName,
+//         Email: req.body.email,
+//         Password: req.body.password
+//       }
+//     })
+//     .spread(function(result, created) {
+//       if (created) {
+//         res.send('User successfully created');
+//       } else {
+//         res.send('This user already exists');
+//       }
+//     });
+// });
 
 module.exports = router;
 
