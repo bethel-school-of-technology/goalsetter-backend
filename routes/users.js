95ed520d432b309461d21b8d47d3b701d868e6b2
Kevin Bjorklund <kbjorklund@Kevins-MBP.attlocal.net>
2019-06-13 17:38:51 -0500

    Updated AuthService in Users

--- routes/users.js
+++ routes/users.js
@@ -62,16 +62,26 @@ router.post('/login', function (req, res, next) {
 
 router.get('/profile', function (req, res, next) {
   let token = req.cookies.jwt;
-  authService.verifyUser(token)
-    .then(user => {
-      if (user) {
-        res.send(JSON.stringify(user));
-      } else {
-        res.status(401);
-        res.send('Must be logged in');
-      }
-    })
+  if (token) {
+    authService.verifyUser(token)
+      .then(user => {
+        if (user) {
+          res.send(JSON.stringify(user));
+        } else {
+          res.status(401);
+          res.send('Invalid authentication token');
+        }
+      });
+  } else {
+    res.status(401);
+    res.send('Must be logged in');
+  }
 });
+
+  router.get('/logout', function (req, res, next) {
+    res.cookie('jwt', "", { expires: new Date(0) });
+    res.send('Logged out');
+    });
 
 /*VICTORS SUGGESTION FOR SIGNUP*/
 //         req.session.currentUser = req.body.Email;
